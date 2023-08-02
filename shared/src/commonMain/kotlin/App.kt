0947549a9e0657eddcd38c6ddd5b35b35ede94b8
Romman Sabbir <rommansabbir@gmail.com>
2023-08-02 18:10:25 +0600

    App ui updated

--- shared/src/commonMain/kotlin/App.kt
+++ shared/src/commonMain/kotlin/App.kt
@@ -1,5 +1,3 @@
-import androidx.compose.animation.AnimatedVisibility
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material.Button
@@ -12,30 +10,22 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import io.kamel.image.KamelImage
-import io.kamel.image.asyncPainterResource
-import org.jetbrains.compose.resources.ExperimentalResourceApi
-import org.jetbrains.compose.resources.painterResource
 
-@OptIn(ExperimentalResourceApi::class)
 @Composable
 fun App() {
     MaterialTheme {
-        var greetingText by remember { mutableStateOf("Hello, World!") }
-        var showImage by remember { mutableStateOf(false) }
+        var showLoginUI by remember { mutableStateOf(false) }
+
         Column(Modifier.fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally) {
             Button(onClick = {
-                greetingText = "Hello, ${getPlatformName()}"
-                showImage = !showImage
+                showLoginUI = !showLoginUI
+
             }) {
-                Text(greetingText)
+                Text("Show Login Button")
             }
-            AnimatedVisibility(showImage) {
-                KamelImage(
-                    asyncPainterResource("https://i1.sndcdn.com/avatars-VCpF7vqiblKh0kGD-Ou3Dnw-t500x500.jpg"),
-                    null
-                )
-            }
+        }
+        if (showLoginUI) {
+            Login()
         }
     }
 }
